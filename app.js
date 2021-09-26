b8b99a9453dbc8d060551f26b4847628a049fb2e
Sveta Haustova <sveta.suprun.hsi@yandex.ru>
2021-09-26 19:34:32 +0300

    исправление ошибок проектной работы 14

--- app.js
+++ app.js
@@ -46,13 +46,14 @@ app.use('*', (req, res, next) => {
 
 app.use(errors());
 
-app.use((err, req, res) => {
+app.use((err, req, res, next) => {
   const { statusCode = 500, message } = err;
   res.status(statusCode).send({
     message: statusCode === 500
       ? 'Ошибка на сервере'
       : message,
   });
+  next();
 });
 
 app.listen(PORT, () => {
