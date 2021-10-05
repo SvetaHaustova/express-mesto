cd3c861c6ddf0255e58bf88377ba42bd64622e2d
Sveta Haustova <sveta.suprun.hsi@yandex.ru>
2021-10-05 23:02:08 +0300

    проектная работа 14

--- models/user.js
+++ models/user.js
@@ -18,7 +18,8 @@ const userSchema = new mongoose.Schema({
   },
   avatar: {
     type: String,
-    default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
+    // eslint-disable-next-line spaced-comment
+    //default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
     validate: {
       validator: (v) => regex.test(v),
       message: 'Некорректный формат ссылки',
